ff3c3ec8f3f96157532b3ce355af4975e09de5b5
Sergii Nikolaichuk <sergiinikolaichuk@gmail.com>
2017-12-03 20:11:08 +0200

    start mysql simple

--- start_mysql_5.7.sh
+++ start_mysql_5.7.sh
@@ -6,18 +6,7 @@ MYSQL_PORT="3306"
 MYSQL_ROOT_PASSWORD="testo"
 if [ ! -d "$DB_PATH"  ]
 then
-    docker run -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -d --name="$CONTAINER" $IMAGE
-    COUNT="0"
-    START_MYSQL="1"
-    while [  "$START_MYSQL" == "1" ]
-    do
-       COUNT=$(( $COUNT + 1 ))
-       sleep 1
-       START_MYSQL=`docker exec -it $CONTAINER sh -c "ps xa | grep initialize-insecure | grep -v grep | wc -l" | sed 's#[\n\r]##g'`
-       echo "Wait start mysql ${COUNT}s"
-    done
-    docker cp -a $CONTAINER:/var/lib/mysql ${DB_PATH}
-    docker stop mysql
-    docker rm mysql
+    mkdir ${DB_PATH}
+    echo "$IMAGE" > ${DB_PATH}/README.txt
 fi
-docker run --restart unless-stopped --name=$CONTAINER  -d -p ${MYSQL_PORT}:3306  -v ${DB_PATH}:/var/lib/mysql  ${IMAGE}
+docker run -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD --restart unless-stopped --name=$CONTAINER  -d -p ${MYSQL_PORT}:3306  -v ${DB_PATH}:/var/lib/mysql  ${IMAGE}
