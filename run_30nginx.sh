56a2cb8fd7aaadb1aebcc24067b67c0671385322
Sergii Nikolaichuk <sergiinikolaichuk@gmail.com>
2017-12-05 18:02:27 +0200

    fix if check dir

--- run_30nginx.sh
+++ run_30nginx.sh
@@ -3,8 +3,7 @@
 IMAGE="nginx"
 docker stop $CONTAINER
 docker rm $CONTAINER
-if [ ! -d $NGINX_LOCAL_DIR  ]
-then
+if [ ! -d $NGINX_LOCAL_DIR  ]; then
     mkdir $NGINX_LOCAL_DIR
     docker run --name="$CONTAINER"  -d -p 80:80 $IMAGE
     docker cp  $CONTAINER:/etc/nginx ${NGINX_LOCAL_DIR}/etc
