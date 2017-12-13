81033c590ff9ccea9d5e62c3aeba0cfd9a7e0371
root <root@kurs.limit.com.ua>
2017-12-13 18:47:40 +0200

    auto add exec php71dev add port for live reload add tmux and screen

--- exec_bash.sh
+++ exec_bash.sh
@@ -2,7 +2,8 @@
 CONTAINER=$1
 if [ -z $CONTAINER ]
 then
-   echo "EMPTY container" >&2
-   exit;
+   CONTAINER="php71dev"
+   echo "EMPTY container set default $CONTAINER" >&2
+   #exit;
 fi
 docker exec -it $CONTAINER bash
--- run_25php71dev.sh
+++ run_25php71dev.sh
@@ -7,10 +7,12 @@ docker rm $CONTAINER
 docker run  -d \
             --restart unless-stopped  \
             -p 9001:9001 \
+            -p 35729:35729 \
             -e PHP_XDEBUG_ENABLED=1 \
             -e XDEBUG_CONFIG="idekey=netbeans-xdebug" \
             -e PHP_IDE_CONFIG="serverName=blog" \
             --name="$CONTAINER" \
+            --link=mysql \
             -v ${NGINX_LOCAL_DIR}/sites:/usr/share/nginx \
             -v $PWD/tmp:/root/tmp \
             $IMAGE
