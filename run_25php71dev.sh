767ba165ff45e5a2e7a1d210511891fa8dbdf13b
Sergii Nikolaichuk <sergiinikolaichuk@gmail.com>
2017-12-14 11:48:28 +0200

    xdebug add automtic host

--- run_25php71dev.sh
+++ run_25php71dev.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 . inc/init.sh
 IMAGE=php71dev
+DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 docker build -t $IMAGE - < Dockerfile_php71dev 
 docker stop $CONTAINER
 docker rm $CONTAINER
 docker run  -d \
             --restart unless-stopped  \
-            -p 9001:9001 \
             -p 35729:35729 \
-            -e PHP_XDEBUG_ENABLED=1 \
-            -e XDEBUG_CONFIG="idekey=netbeans-xdebug" \
-            -e PHP_IDE_CONFIG="serverName=blog" \
+            -e XDEBUG_CONFIG="remote_host=$DOCKERHOST" \
             --name="$CONTAINER" \
             --link=mysql \
             -v ${NGINX_LOCAL_DIR}/sites:/usr/share/nginx \
